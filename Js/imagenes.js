17f12be626891c4ff9b20a51c35f1d06f5b122b0
Alejandra Orozco Aguilar <ale12orozco@gmail.com>
2023-01-14 21:07:58 -0600

    Cambio en el tamaño de imagen

--- Js/imagenes.js
+++ Js/imagenes.js
@@ -8,6 +8,7 @@ imagen.src = "img/animal ("+numImagen+").jpg";  //Cambia la imagen por una nueva
 
 //Cambia la imagen por una nueva imagen aleatoria
 function CambiarImagen() {
+    LimpiarCampos();
     let imagen = document.getElementById("imagen");
     let numImagen = Math.floor(Math.random() * 101); //Genera un numero aleatorio entre 0 y 100
     imagen.src = "img/animal ("+numImagen+").jpg";  //Cambia la imagen por una nueva 
